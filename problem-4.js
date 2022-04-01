d7186453bbbd85d25d98e83f9746243ec5a636b6
Mark-Kohrman <mark.kohrman@gmail.com>
2022-03-31 21:26:20 -0500

    add spacing

--- problem-4.js
+++ problem-4.js
@@ -1,35 +1,35 @@
 //Project Euler Problem #4
 
 function largestPalindrome(multiplier, decrementMultiplier) {
-  while (multiplier > 899) {
-    decrementMultiplier = multiplier;
-    while (decrementMultiplier > 899) {
-      let product = decrementMultiplier * multiplier;
-      let productString = product.toString();
-      let reversedProductString = reverseString(productString);
+	while (multiplier > 899) {
+		decrementMultiplier = multiplier;
+		while (decrementMultiplier > 899) {
+		let product = decrementMultiplier * multiplier;
+		let productString = product.toString();
+		let reversedProductString = reverseString(productString);
 
-      if (reversedProductString === productString) {
-        console.log(product);
-        return product;
-      }
-      decrementMultiplier --;
-    }
-    multiplier --;
-  }
-}
+		if (reversedProductString === productString) {
+			console.log(product);
+			return product;
+		}
+		decrementMultiplier --;
+		}
+		multiplier --;
+	}
+	}
 
-largestPalindrome(999,999);
+	largestPalindrome(999,999);
 
-function reverseString(productString) {
-  let i = 0;
-  let j = productString.length - 1;
-  let reversedString = "";
+	function reverseString(productString) {
+	let i = 0;
+	let j = productString.length - 1;
+	let reversedString = "";
 
-  while (i < productString.length) {
-    reversedString += productString[j];
-    j --;
-    i ++;
-  }
+	while (i < productString.length) {
+		reversedString += productString[j];
+		j --;
+		i ++;
+	}
 
-  return reversedString;
+	return reversedString;
 }
