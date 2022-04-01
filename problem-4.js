be68966017a5cc4ba64b3471c9c7144bc75e484b
Mark-Kohrman <mark.kohrman@gmail.com>
2022-03-31 21:31:42 -0500

    spacing

--- problem-4.js
+++ problem-4.js
@@ -3,24 +3,27 @@
 function largestPalindrome(multiplier, decrementMultiplier) {
 	while (multiplier > 899) {
 		decrementMultiplier = multiplier;
-		while (decrementMultiplier > 899) {
-		let product = decrementMultiplier * multiplier;
-		let productString = product.toString();
-		let reversedProductString = reverseString(productString);
 
-		if (reversedProductString === productString) {
-			console.log(product);
-			return product;
+		while (decrementMultiplier > 899) {
+			let product = decrementMultiplier * multiplier;
+			let productString = product.toString();
+			let reversedProductString = reverseString(productString);
+
+			if (reversedProductString === productString) {
+				console.log(product);
+				return product;
+			}
+
+			decrementMultiplier --;
 		}
-		decrementMultiplier --;
-		}
+
 		multiplier --;
 	}
-	}
+}
 
-	largestPalindrome(999,999);
+largestPalindrome(999,999);
 
-	function reverseString(productString) {
+function reverseString(productString) {
 	let i = 0;
 	let j = productString.length - 1;
 	let reversedString = "";
